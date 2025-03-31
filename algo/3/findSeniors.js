16a5cf23f62e4709bbc499991053e6b579ded81d
Alexandra PASTOR <pastoralex@free.fr>
2025-03-31 14:00:38 +0200

    modif css optionnel

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -25,8 +25,42 @@ const persons = [
 
 */
 
+const persons = [
+	{ name: "Mary", experience: 2, job: "web dev" },
+	{ name: "Tony", experience: 6, job: "data analyst" },
+	{ name: "John", experience: 2, job: "data analyst" },
+	{ name: "Jane", experience: 6, job: "web dev" },
+	{ name: "Maggie", experience: 2, job: "web dev" },
+	{ name: "Leonardo", experience: 2, job: "data analyst" },
+	{ name: "Carla", experience: 4, job: "data analyst" },
+	{ name: "Mickael", experience: 7, job: "web dev" },
+	{ name: "Penelope", experience: 7, job: "web dev" },
+	{ name: "Homer", experience: 5, job: "data analyst" },
+	{ name: "Leonardo", experience: 2, job: "data analyst" },
+	{ name: "Carla", experience: 4, job: "web dev" },
+	{ name: "Lisa", experience: 3, job: "web dev" },
+	{ name: "Millie", experience: 5, job: "data analyst" },
+	{ name: "Penelope", experience: 7, job: "web dev" },
+];
+
 function findSeniors(persons) {
-  // Your code here !
-}
+	const tab1 = [];
+  const tab2 = [];
+
+  for (let i = 0; i < persons.length; i++) {
+    const person = persons[i];
+
+  if(person.experience>=5 && person.job==="web dev"){
+    tab1.push(person);
+  };
+
+  else if(person.job === "data analyst") {
+    tab2.push(person);
+  }
+  }
+  return {tab1, tab2};
+};
+
+
 
 module.exports = findSeniors;

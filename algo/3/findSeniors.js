efdb08f1c182dd9d6d1330528a390f1051585d07
Belovitchy <belo38690@gmail.com>
2025-03-31 12:09:26 +0200

    finish bonus css

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -4,28 +4,34 @@ lui-même contenant deux sous-tableaux :
 - Le premier contient uniquement des devs web seniors
 - Le second ne contient que des data analysts seniors
 (Étant donné qu'une personne est senior si elle a 5 ans d'expérience ou plus)
-
-const persons = [
-  { name: 'Mary', experience: 2, job: 'web dev' },
-  { name: 'Tony', experience: 6, job: 'data analyst' },
-  { name: 'John', experience: 2, job: 'data analyst' },
-  { name: 'Jane', experience: 6, job: 'web dev' },
-  { name: 'Maggie', experience: 2, job: 'web dev' },
-  { name: 'Leonardo', experience: 2, job: 'data analyst' },
-  { name: 'Carla', experience: 4, job: 'data analyst' },
-  { name: 'Mickael', experience: 7, job: 'web dev' },
-  { name: 'Penelope', experience: 7, job: 'web dev' },
-  { name: 'Homer', experience: 5, job: 'data analyst' },
-  { name: 'Leonardo', experience: 2, job: 'data analyst' },
-  { name: 'Carla', experience: 4, job: 'web dev' },
-  { name: 'Lisa', experience: 3, job: 'web dev' },
-  { name: 'Millie', experience: 5, job: 'data analyst' },
-  { name: 'Penelope', experience: 7, job: 'web dev' },
-];
-
 */
 
+const persons = [
+  { name: "Mary", experience: 2, job: "web dev" },
+  { name: "Tony", experience: 6, job: "data analyst" },
+  { name: "John", experience: 2, job: "data analyst" },
+  { name: "Jane", experience: 6, job: "web dev" },
+  { name: "Maggie", experience: 2, job: "web dev" },
+  { name: "Leonardo", experience: 2, job: "data analyst" },
+  { name: "Carla", experience: 4, job: "data analyst" },
+  { name: "Mickael", experience: 7, job: "web dev" },
+  { name: "Penelope", experience: 7, job: "web dev" },
+  { name: "Homer", experience: 5, job: "data analyst" },
+  { name: "Leonardo", experience: 2, job: "data analyst" },
+  { name: "Carla", experience: 4, job: "web dev" },
+  { name: "Lisa", experience: 3, job: "web dev" },
+  { name: "Millie", experience: 5, job: "data analyst" },
+  { name: "Penelope", experience: 7, job: "web dev" },
+];
+
 function findSeniors(persons) {
+  const devSeniors = persons.filter(
+    (person) => person.job === "web dev" && person.experience >= 5
+  );
+  const dataSenior = persons.filter(
+    (person) => person.job === "data analyst" && person.experience >= 5
+  );
+  return [devSeniors, dataSenior];
   // Your code here !
 }
 

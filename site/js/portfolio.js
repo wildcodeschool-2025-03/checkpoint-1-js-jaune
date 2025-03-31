bca94e62249a52c52ff7cb01319a6fbd906297e3
Davidlevy13 <levy.david1993@gmail.com>
2025-03-31 12:16:25 +0200

    début JS site

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,16 @@
 const avatar = document.querySelector(".avatar");
 
 avatar.addEventListener('click', function () {
-    avatar.src = ../avatar.svg;
+    avatar.src = "image/avatar.svg";
     avatar.alt = "new avatar"
 });
+
+
+    const firstName = document.querySelectorByID('#firstname');
+    const button = document.querySelector('textbutton');
+    button.addEventListener('click', function (){
+    const newText = prompt('Quel est votre nom ?', firstName, textContent)
+    const textColor = prompt('Changer la couleur')
+  
+    });
+
